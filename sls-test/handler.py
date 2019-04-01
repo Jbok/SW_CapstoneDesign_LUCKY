d1d9c76a618f59f9b53dbb20ad030a27739e0a27
sipsyong <0614bok@gmail.com>
2019-04-01 03:49:35 -0700

    sls-test
    import openCV and numpy at sls

---
+++ sls-test/handler.py
@@ -0,0 +1,37 @@
+import json
+import numpy as np
+import cv2
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
+
+def main(event, context):
+    a = np.arange(15).reshape(3, 5)
+
+    print("Your numpy array:")
+    print(a)
+    print(cv2.__version__)
+
+
+if __name__ == "__main__":
+    main('', '')
